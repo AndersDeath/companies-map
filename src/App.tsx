b9128f3529874b96fe419f33fc60646dbe86a14b
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-05-06 14:35:24 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import './App.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
+import logo from './logo.svg';
+
+import L from 'leaflet';
+
+const iconPerson = new L.Icon({
+    iconUrl: logo,
+    iconRetinaUrl: logo,
+    iconSize: new L.Point(60, 75),
+    className: 'leaflet-div-icon'
+});
+
+export { iconPerson };
+
+
 function App() {
   const position: any = [51.505, -0.09];
 
@@ -13,7 +27,7 @@ function App() {
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
+    <Marker position={position} icon={iconPerson}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
